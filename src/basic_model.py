5068258f196025416a34407936142215958aad9c
Ubuntu <ubuntu@ip-172-31-44-81.ec2.internal>
2018-12-13 17:55:24 +0000

    Bugfixes to helper files

--- src/basic_model.py
+++ src/basic_model.py
@@ -1,8 +1,8 @@
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, Flatten
 
-import src.constants as const
-import src.preprocessing_helper as pre
+import constants
+from preprocessing_helper import *
 
 
 class BasicModel:
@@ -14,18 +14,18 @@ class BasicModel:
         self.model = Sequential()
 
         # add model layers
-        self.model.add(Conv2D(16, kernel_size=const.IMG_PATCH_SIZE, activation='relu',
-                              input_shape=(const.IMG_PATCH_SIZE, const.IMG_PATCH_SIZE, const.NUM_CHANNELS)))
+        self.model.add(Conv2D(16, kernel_size=constants.IMG_PATCH_SIZE, activation='relu',
+                              input_shape=(constants.IMG_PATCH_SIZE, constants.IMG_PATCH_SIZE, constants.NUM_CHANNELS)))
         self.model.add(Flatten())
         self.model.add(Dense(2, activation='softmax'))
 
         # compile model using accuracy to measure model performance
         self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-    def fit(self, train_data, train_labels, epochs=100):
+    def train(self, train_data, train_labels, epochs=100):
 
         # Step 1: Either put a class weight or remove some data from train set to have 50 / 50 data per class
-        X, Y = pre.get_equal_train_set_per_class(train_data, train_labels)
+        X, Y = get_equal_train_set_per_class(train_data, train_labels)
 
         # Step 2: Other things
 
--- src/preprocessing_helper.py
+++ src/preprocessing_helper.py
@@ -1,4 +1,84 @@
 import numpy as np
+import os
+import sys
+import matplotlib.image as mpimg
+import constants
+import numpy
+
+
+# Extract patches from a given image
+def img_crop(im, w, h):
+    list_patches = []
+    imgwidth = im.shape[0]
+    imgheight = im.shape[1]
+    is_2d = len(im.shape) < 3
+    for i in range(0, imgheight, h):
+        for j in range(0, imgwidth, w):
+            if is_2d:
+                im_patch = im[j:j + w, i:i + h]
+            else:
+                im_patch = im[j:j + w, i:i + h, :]
+            list_patches.append(im_patch)
+    return list_patches
+
+
+def extract_data(filename, num_images):
+    """Extract the images into a 4D tensor [image index, y, x, channels].
+    Values are rescaled from [0, 255] down to [-0.5, 0.5].
+    """
+    imgs = []
+    for i in range(1, num_images + 1):
+        imageid = "satImage_%.3d" % i
+        image_filename = filename + imageid + ".png"
+        if os.path.isfile(image_filename):
+            print('Loading ' + image_filename)
+            img = mpimg.imread(image_filename)
+            imgs.append(img)
+        else:
+            print('File ' + image_filename + ' does not exist')
+
+    num_images = len(imgs)
+    IMG_WIDTH = imgs[0].shape[0]
+    IMG_HEIGHT = imgs[0].shape[1]
+    N_PATCHES_PER_IMAGE = (IMG_WIDTH / constants.IMG_PATCH_SIZE) * (IMG_HEIGHT / constants.IMG_PATCH_SIZE)
+
+    img_patches = [img_crop(imgs[i], constants.IMG_PATCH_SIZE, constants.IMG_PATCH_SIZE) for i in range(num_images)]
+    data = [img_patches[i][j] for i in range(len(img_patches)) for j in range(len(img_patches[i]))]
+
+    return numpy.asarray(data)
+
+
+# Assign a label to a patch v
+def value_to_class(v):
+    df = numpy.sum(v)
+    if df > constants.FOREGROUND_THRESHOLD:
+        return [0, 1]
+    else:
+        return [1, 0]
+
+
+# Extract label images
+def extract_labels(filename, num_images):
+    """Extract the labels into a 1-hot matrix [image index, label index]."""
+    gt_imgs = []
+    for i in range(1, num_images + 1):
+        imageid = "satImage_%.3d" % i
+        image_filename = filename + imageid + ".png"
+        if os.path.isfile(image_filename):
+            print('Loading ' + image_filename)
+            img = mpimg.imread(image_filename)
+            gt_imgs.append(img)
+        else:
+            print('File ' + image_filename + ' does not exist')
+
+    num_images = len(gt_imgs)
+    gt_patches = [img_crop(gt_imgs[i], constants.IMG_PATCH_SIZE, constants.IMG_PATCH_SIZE) for i in range(num_images)]
+    data = numpy.asarray([gt_patches[i][j] for i in range(len(gt_patches)) for j in range(len(gt_patches[i]))])
+    labels = numpy.asarray([value_to_class(numpy.mean(data[i])) for i in range(len(data))])
+
+    # Convert to dense 1-hot representation.
+    return labels.astype(numpy.float32)
+
 
 def get_equal_train_set_per_class(train_data, train_labels):
     """ In charge of making sure training data is equally split between both classes """
