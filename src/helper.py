883da1811a223f1caa3e7fd8922163b7b6cf7ba0
Yann Vonlanthen <yann.vonlanthen@epfl.ch>
2018-12-13 17:52:53 +0100

    New pipeline, does not compile

--- src/helper.py
+++ src/helper.py
@@ -1,1 +1,22 @@
 """ This file contains general helper functions """
+
+import matplotlib.image as mpimg
+
+# Helper functions
+def load_image(infilename):
+    data = mpimg.imread(infilename)
+    return data
+
+def img_crop(im, w, h):
+    list_patches = []
+    imgwidth = im.shape[0]
+    imgheight = im.shape[1]
+    is_2d = len(im.shape) < 3
+    for i in range(0,imgheight,h):
+        for j in range(0,imgwidth,w):
+            if is_2d:
+                im_patch = im[j:j+w, i:i+h]
+            else:
+                im_patch = im[j:j+w, i:i+h, :]
+            list_patches.append(im_patch)
+    return list_patches
--- src/preprocessing_helper.py
+++ src/preprocessing_helper.py
@@ -1,8 +1,9 @@
+import numpy as np
 
-def get_equal_train_set_per_class:
+def get_equal_train_set_per_class(train_data, train_labels):
     """ In charge of making sure training data is equally split between both classes """
-    c
-    0 = 0
+
+    c0 = 0
     c1 = 0
     for i in range(len(train_labels)):
         if train_labels[i][0] == 1:
@@ -16,8 +17,8 @@ def get_equal_train_set_per_class:
     idx0 = [i for i, j in enumerate(train_labels) if j[0] == 1]
     idx1 = [i for i, j in enumerate(train_labels) if j[1] == 1]
     new_indices = idx0[0:min_c] + idx1[0:min_c]
-    print (len(new_indices))
-    print (train_data.shape)
+    print(len(new_indices))
+    print(train_data.shape)
     train_data = train_data[new_indices,:,:,:]
     train_labels = train_labels[new_indices]
 
@@ -32,3 +33,17 @@ def get_equal_train_set_per_class:
         else:
             c1 = c1 + 1
     print ('Number of data points per class: c0 = ' + str(c0) + ' c1 = ' + str(c1))
+    return train_data, train_labels
+
+
+# pad an image (with 3RGB dim with) of padSize by mirroring border
+def pad_image(img, padSize):
+    return np.lib.pad(img, ((padSize, padSize), (padSize, padSize), (0, 0)), 'reflect')
+
+
+# pad an ground_truth image
+def pad_gt_img(img, padSize):
+    return np.lib.pad(img, ((padSize, padSize), (padSize, padSize)), 'reflect')
+
+
+# plt.imshow((pad_image(imgs[0], 100)), cmap='Greys_r')
